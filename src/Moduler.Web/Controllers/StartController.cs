eda22f3a205740e980806055bc23cdf5842b1811
Andreas <andreas@thecodingbug.com>
2013-08-04 20:13:05 +0200

    Initial commit

---
+++ src/Moduler.Web/Controllers/StartController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Moduler.Web.Controllers
+{
+    public class StartController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Child()
+        {
+            return PartialView();
+        }
+
+        public ActionResult SuperChild()
+        {
+            Thread.Sleep(100);
+            return PartialView();
+        }
+
+    }
+}
