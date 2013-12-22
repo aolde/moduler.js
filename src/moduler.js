0f1d56de5c376c7d329368889cb8403ea31d04b8
Andreas <andreas@thecodingbug.com>
2013-12-22 13:20:05 +0100

    Removed unused code

--- src/moduler.js
+++ src/moduler.js
@@ -208,49 +208,10 @@
         };
     })();
     
+    // run mo.init at dom load
     $(mo.init);
 
     win.mo = mo;
     win.moduler = mo.module;
     
 })(window, document, jQuery);
-
-
-
-//// suppress console.log errors in unsupported browsers
-//if (!(window.console && console.log && console.groupCollapsed)) {
-//    (function () {
-//        var d = function () { };
-//        var b = ["assert", "clear", "count", "debug", "dir", "dirxml", "error", "exception", "group", "groupCollapsed", "groupEnd", "info", "log", "markTimeline", "profile", "profileEnd", "markTimeline", "table", "time", "timeEnd", "timeStamp", "trace", "warn"];
-//        var c = b.length;
-//        var a = window.console = {};
-//        while (c--) {
-//            a[b[c]] = d;
-//        }
-//    }());
-//};
-
-
-//if (!Function.prototype.bind) {
-//    Function.prototype.bind = function (oThis) {
-//        if (typeof this !== "function") {
-//            // closest thing possible to the ECMAScript 5 internal IsCallable function
-//            throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-//        }
-
-//        var aArgs = Array.prototype.slice.call(arguments, 1),
-//            fToBind = this,
-//            fNOP = function () { },
-//            fBound = function () {
-//                return fToBind.apply(this instanceof fNOP && oThis
-//                                       ? this
-//                                       : oThis,
-//                                     aArgs.concat(Array.prototype.slice.call(arguments)));
-//            };
-
-//        fNOP.prototype = this.prototype;
-//        fBound.prototype = new fNOP();
-
-//        return fBound;
-//    };
-//}
