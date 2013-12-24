01a252b414d5385e7e0d950a8df147d1f661fd1f
simplyio <andreas@simply.io>
2013-12-24 15:54:26 +0100

    Added demo page for formPoster module

---
+++ src/modules/jsonResponseHandler.js
@@ -0,0 +1,16 @@
+﻿(function ($) {
+    "use strict";
+
+    var jsonResponseHandler = moduler('jsonResponseHandler', {
+        defaults: {
+            response: null,
+            fromModule: null
+        },
+
+        init: function (module) {
+            alert(JSON.stringify(module.settings.response.args));
+            console.log('RESPONSE!', module.settings.response);
+        }
+    });
+
+})(jQuery);
