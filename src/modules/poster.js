e82692398cc8d0021be7ecca526ebdd0cbb39e2c
simplyio <andreas@simply.io>
2013-12-30 18:35:45 +0100

    Added poster.js module

---
+++ src/modules/poster.js
@@ -0,0 +1,55 @@
+(function () {
+    "use strict";
+
+    var moduleObj = moduler('poster', {
+        defaults: {
+            url: null,
+            event: 'click',
+            data: undefined,
+            contentElement: null,
+            responseModule: null,
+            httpMethod: 'POST',
+            loadingCssClass: 'loading'
+        },
+        
+        init: function (module) {
+            module.$element.on(module.settings.event, mo.data(module), moduleObj.listen.fireRequest);
+        },
+
+        listen: {
+            fireRequest: mo.event(function (module, e) {
+                var settings = module.settings,
+                    $contentElement = settings.contentElement !== null ? $(settings.contentElement) : module.$element;
+
+                e.preventDefault();
+                module.$element.addClass(settings.loadingCssClass);
+
+                // send request
+                $.ajax({
+                    type: settings.httpMethod,
+                    url: settings.url,
+                    data: settings.data ? settings.data : {}
+                }).always(function () {
+                    module.$element.removeClass(settings.loadingCssClass);
+                }).done(function (response, status, xhr) {
+                    if (settings.responseModule) {
+                        mo.utils.removeModuleFromElement($contentElement, settings.responseModule);
+                        mo.utils.addModuleToElement($contentElement, settings.responseModule, { response: response });
+                    } else if (settings.contentElement && xhr.getResponseHeader('content-type').indexOf('text/html') !== -1) {
+                        $contentElement.html(response);
+                    }
+
+                    module.$element.trigger('poster-done', { response: response });
+                }).error(function () {
+                    module.$element.trigger('poster-error');
+                });
+    
+            })
+        },
+
+        destroy: function (module) {
+            module.$element.off(module.settings.event, moduleObj.listen.fireRequest);
+        }
+    });
+    
+})();
