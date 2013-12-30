48dbf5ceda1b9e3059d5ea387d0418df2e0310a0
simplyio <andreas@simply.io>
2013-12-30 17:47:15 +0100

    Added confirm.js module

---
+++ src/modules/confirm.js
@@ -0,0 +1,31 @@
+(function () {
+    "use strict";
+
+    var m = moduler('confirm', {
+        defaults: {
+            message: 'Are you sure you want to perform this action?',
+            event: 'click'
+        },
+        
+        init: function (module) {
+            module.$element.on(module.settings.event, mo.data(module), m.listen.showConfirm);
+        },
+
+        listen: {
+            showConfirm: mo.event(function (module, e) {
+                e.preventDefault();
+
+                if (window.confirm(module.settings.message)) {
+                    module.$element.trigger('confirm-yes');
+                } else {
+                    module.$element.trigger('confirm-no');
+                }
+            })
+        },
+        
+        destroy: function () {
+            module.$element.off(module.settings.event, m.listen.showConfirm);
+        }
+    });
+    
+})();
