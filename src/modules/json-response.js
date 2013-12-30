2519e90912d20afd62c5ff1c87fdb7da38062990
simplyio <andreas@simply.io>
2013-12-30 22:05:30 +0100

    Rename jsonResponseHandler to json-response, + cleanup

---
+++ src/modules/json-response.js
@@ -0,0 +1,24 @@
+﻿(function ($) {
+    "use strict";
+    
+    /* example of how a response module could look */
+
+    var moduleObj = moduler('json-response', {
+        defaults: {
+            response: null
+        },
+
+        init: function (module) {
+            var formatValues = "",
+                response = module.settings.response;
+
+            for (var prop in response) {
+                formatValues += prop + "=" + JSON.stringify(response[prop]) + "\n\n"; 
+            }
+
+            alert(formatValues);
+            console.log('RESPONSE!', module.settings.response);
+        }
+    });
+
+})(jQuery);
