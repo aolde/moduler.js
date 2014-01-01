397859466564fd956d37ced02d5466b56337fb0c
simplyio <andreas@simply.io>
2014-01-01 23:39:39 +0100

    Added simple toggle class/visibility module

---
+++ src/modules/toggler.js
@@ -0,0 +1,34 @@
+﻿(function ($) {
+    "use strict";
+
+    var moduleObj = moduler('toggler', {
+        defaults: {
+            event: 'click',
+            contentElement: null,
+            cssClass: 'hide'
+        },
+
+        init: function (module) {
+            module.$contentElement = $(module.settings.contentElement);
+            module.$element.on(module.settings.event, module, moduleObj.listen.toggleVisibility);
+        },
+
+        listen: {
+            show: mo.event(function (module) {
+                module.$contentElement.removeClass(module.settings.cssClass);
+                module.$contentElement.trigger('toggler-shown');
+            }),
+
+            hide: mo.event(function (module) {
+                module.$contentElement.addClass(module.settings.cssClass);
+                module.$contentElement.trigger('toggler-hidden');
+            }),
+
+            toggleVisibility: mo.event(function (module) {
+                module.$contentElement.toggleClass(module.settings.cssClass);
+                module.$contentElement.trigger('toggler-toggeled');
+            })
+        }
+    });
+
+})(jQuery);
