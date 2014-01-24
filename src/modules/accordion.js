48f23ff125c8cf2d6cafc894ca53ea107120e9ae
simplyio <andreas@simply.io>
2014-01-24 15:42:55 +0000

    Fix indentation in accordion and dispatcher

--- src/modules/accordion.js
+++ src/modules/accordion.js
@@ -3,76 +3,76 @@
 
     var moduleObj = moduler('accordion', {
         defaults: {
-        	event: 'click',
-        	headerClass: 'accordion-header',
-        	sectionClass: 'accordion-section',
-        	activeClass: 'is-active',
-        	mode: 'accordion', // accordion|toggle
-        	slide: true,
-        	slideDelay: 400,
-        	expandFirst: false
+            event: 'click',
+            headerClass: 'accordion-header',
+            sectionClass: 'accordion-section',
+            activeClass: 'is-active',
+            mode: 'accordion', // accordion|toggle
+            slide: true,
+            slideDelay: 400,
+            expandFirst: false
         },
         
         init: function (module) {
-        	module.$headers = module.$element.find('.' + module.settings.headerClass);
-        	module.$sections = module.$element.find('.' + module.settings.sectionClass);
+            module.$headers = module.$element.find('.' + module.settings.headerClass);
+            module.$sections = module.$element.find('.' + module.settings.sectionClass);
 
-        	if (module.settings.slide) {
-        		module.$sections.hide();
-        	}
+            if (module.settings.slide) {
+                module.$sections.hide();
+            }
 
             module.$headers.on(module.settings.event, module, moduleObj.listen.expandSection);
 
-        	module.$element.addClass('js-ready');
+            module.$element.addClass('js-ready');
 
-        	if (module.settings.expandFirst) {
-        		module.$headers.first().trigger(module.settings.event);
-        	}
+            if (module.settings.expandFirst) {
+                module.$headers.first().trigger(module.settings.event);
+            }
         },
         
         listen: {
-        	expandAll: mo.event(function (module) {
-    			module.$headers.addClass(module.settings.activeClass).trigger('accordion-expand');
-    			module.$sections.addClass(module.settings.activeClass).trigger('accordion-expand');
-    			
-    			if (module.settings.slide) {
-    				module.$sections.slideDown(module.settings.slideDelay);
-    			}
-        	}),
+            expandAll: mo.event(function (module) {
+                module.$headers.addClass(module.settings.activeClass).trigger('accordion-expand');
+                module.$sections.addClass(module.settings.activeClass).trigger('accordion-expand');
+                
+                if (module.settings.slide) {
+                    module.$sections.slideDown(module.settings.slideDelay);
+                }
+            }),
 
-        	collapseAll: mo.event(function (module, e) {
-        		module.$headers.removeClass(module.settings.activeClass).trigger('accordion-collapse');
-    			module.$sections.removeClass(module.settings.activeClass).trigger('accordion-collapse');
-        	
-        		if (module.settings.slide) {
-    				module.$sections.slideUp(module.settings.slideDelay);
-    			}
-        	}),
+            collapseAll: mo.event(function (module, e) {
+                module.$headers.removeClass(module.settings.activeClass).trigger('accordion-collapse');
+                module.$sections.removeClass(module.settings.activeClass).trigger('accordion-collapse');
+            
+                if (module.settings.slide) {
+                    module.$sections.slideUp(module.settings.slideDelay);
+                }
+            }),
 
-    		expandSection: mo.event(function (module, e) {
-    			e.preventDefault();
-    			
-    			var $header = $(e.target),
-    				settings = module.settings,
-    				headerIndex = module.$headers.index($header),
-    				$section = module.$sections.eq(headerIndex);
+            expandSection: mo.event(function (module, e) {
+                e.preventDefault();
+                
+                var $header = $(e.target),
+                    settings = module.settings,
+                    headerIndex = module.$headers.index($header),
+                    $section = module.$sections.eq(headerIndex);
 
-    			if (settings.mode == 'accordion') {
-    				module.$headers.not($header).removeClass(settings.activeClass).trigger('accordion-collapse');
-    				module.$sections.not($section).removeClass(settings.activeClass).trigger('accordion-collapse');
-    				
-    				if (settings.slide) {
-	    				module.$sections.not($section).slideUp(settings.slideDelay);
-	    			}
-    			}
+                if (settings.mode == 'accordion') {
+                    module.$headers.not($header).removeClass(settings.activeClass).trigger('accordion-collapse');
+                    module.$sections.not($section).removeClass(settings.activeClass).trigger('accordion-collapse');
+                    
+                    if (settings.slide) {
+                        module.$sections.not($section).slideUp(settings.slideDelay);
+                    }
+                }
 
-    			$header.toggleClass(settings.activeClass).trigger('accordion-expand');
-				$section.toggleClass(settings.activeClass).trigger('accordion-expand');
-    			
-    			if (settings.slide) {
-	    			$section.stop(true).slideToggle(settings.slideDelay);
-    			}
-    		})
+                $header.toggleClass(settings.activeClass).trigger('accordion-expand');
+                $section.toggleClass(settings.activeClass).trigger('accordion-expand');
+                
+                if (settings.slide) {
+                    $section.stop(true).slideToggle(settings.slideDelay);
+                }
+            })
         },
         
         destroy: function (module) {
--- src/modules/dispatcher.js
+++ src/modules/dispatcher.js
@@ -3,47 +3,47 @@
 
     var moduleObj = moduler('dispatcher', {
         defaults: {
-        	listenEvent: 'click', // the event to listen for. (required)
-        	listenSelector: null, // selector for elements to listen for the event (optional)
+            listenEvent: 'click', // the event to listen for. (required)
+            listenSelector: null, // selector for elements to listen for the event (optional)
 
-        	triggerEvent: '', // event name to trigger when listenEvent fires. (required)
-        	triggerSelector: null, // selector for elements to trigger the event. (optional)
-        	triggerContext: null, // the context in which it will try to find elements through triggerSelector. (optional)
+            triggerEvent: '', // event name to trigger when listenEvent fires. (required)
+            triggerSelector: null, // selector for elements to trigger the event. (optional)
+            triggerContext: null, // the context in which it will try to find elements through triggerSelector. (optional)
 
-        	preventDefault: true // whether to preventDefault when event fires.
+            preventDefault: true // whether to preventDefault when event fires.
         },
         
         init: function (module) {
-        	var settings = module.settings;
+            var settings = module.settings;
 
             module.$element.on(settings.listenEvent, settings.listenSelector, module, moduleObj.eventFired);
         },
 
         eventFired: mo.event(function (module, e) {
-        	var settings = module.settings;
+            var settings = module.settings;
 
-        	if (settings.preventDefault) {
-        		e.preventDefault();
-        	}
+            if (settings.preventDefault) {
+                e.preventDefault();
+            }
 
-        	if (settings.triggerSelector) {
-        		var $container = moduleObj.getContext(module);
-        		$container.find(settings.triggerSelector).trigger(settings.triggerEvent);
-        	} else {
-        		module.$element.trigger(settings.triggerEvent);
-        	}
+            if (settings.triggerSelector) {
+                var $container = moduleObj.getContext(module);
+                $container.find(settings.triggerSelector).trigger(settings.triggerEvent);
+            } else {
+                module.$element.trigger(settings.triggerEvent);
+            }
         }),
 
         getContext: function (module) {
-        	var triggerContext = module.settings.triggerContext;
-        	
-        	if (triggerContext) {
-        		if (triggerContext == 'parent') {
-        			return module.$element.parent(); 
-        		}
-        		return $(triggerContext);
-        	}
-        	return module.$element;
+            var triggerContext = module.settings.triggerContext;
+            
+            if (triggerContext) {
+                if (triggerContext == 'parent') {
+                    return module.$element.parent(); 
+                }
+                return $(triggerContext);
+            }
+            return module.$element;
         },
         
         destroy: function (module) { 
